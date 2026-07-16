8d111f599839533a4f8abd9733fbc7b5eb481d8c
Nahida Rojoa <nahida.rojoa@spoonconsulting.com>
2026-07-16 17:23:52 +0400

    test

--- download_script.py
+++ download_script.py
@@ -48,7 +48,7 @@ def download_mauritius_budget():
         pdf_response = requests.get(budget_link, headers=headers)
         pdf_response.raise_for_status()
         
-        # 6. Create a specific folder named 'Budgets' if it doesn't exist
+        # 6. Create a specific folder named 'PDF' if it doesn't exist
         output_folder = os.path.join(os.getcwd(), "PDF")
         if not os.path.exists(output_folder):
             os.makedirs(output_folder)
